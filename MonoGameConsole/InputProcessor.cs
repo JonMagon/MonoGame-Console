e698a8045b6a5b8a1be2c81002b02b9d74b07da1
Dmitry Sidorov <jonmagon@gmail.com>
2017-09-17 11:29:43 +0300

    fixed index out of bounds

--- MonoGameConsole/InputProcessor.cs
+++ MonoGameConsole/InputProcessor.cs
@@ -36,23 +36,10 @@ namespace MonoGameConsole
             Out = new List<OutputLine>();
             Buffer = new OutputLine("", OutputLineType.Command);
 
-            Form winGameWindow = GetGameWindow();
+            Form winGameWindow = (Form)Control.FromHandle(window.Handle);
             winGameWindow.KeyPress += form_KeyPress;
             winGameWindow.KeyDown += EventInput_KeyDown;
         }
-
-        /// <summary>
-        /// Force application to find current app window controls with System.Windows.Forms (Windows)
-        /// </summary>
-        /// <returns></returns>
-        private Form GetGameWindow()
-        {
-            FormCollection winFormsWindow = Application.OpenForms;
-
-            return (Form)Control.FromHandle(winFormsWindow[0].Handle);
-
-        }
-
 
         private void form_KeyPress(Object sender, KeyPressEventArgs e)
         {
